409d30c843a7801d176b41fe620e01d7470208b7
danielavallecillo <daniela2003vf@gmail.com>
2026-07-01 16:45:14 -0600

    cree clase M_CATEGORIA, ah y arregle el context

---
+++ P/M_CATEGORIA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3_Paradigmas.P
+{
+    internal class M_CATEGORIA
+    {
+        public int id { get; set; }
+        public string categoria { get; set; }
+    }
+}
